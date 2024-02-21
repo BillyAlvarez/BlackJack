afdae92f2be34ba6fd0322e03d69c26f776ab752
Billy <billyjag1@gmail.com>
2024-02-21 02:57:18 -0300

    se agregan logica valor Carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -9,6 +9,7 @@ let deck = [];
 const tipos = ['C','D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
+//esta funcion crea una  nueva baraja
 const crearDeck = ()=> {
     for (let i = 2; i <= 10; i++){
         for (let tipo of tipos) {
@@ -22,11 +23,36 @@ const crearDeck = ()=> {
         }
     }
 
-    console.log(deck);
+    // console.log(deck);
     deck = _.shuffle(deck);
     console.log(deck);
     return deck
 }
 
  crearDeck();
+
+
+//esta funcion me permite tomar una carta
+
+const pedirCarta = () => {
+
+    if ( deck.length === 0 ){
+        throw 'No hay mas cartas en el deck!'
+    }
+
+    const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta); //carta debe ser la que se quito de la baraja
+    return carta;
+}
+
+//pedirCarta();
+
+const valorCarta = (carta)=> {
+    const valor = carta.substring(0, carta.length -1);
+    return (isNaN( valor)) ? (valor ==='A') ? 11 : 10 :valor * 1;
+}
+
+console.log(valorCarta( pedirCarta()));
 
